576a3b9441f4afe4e3c6e88b24d94d8641594d34
Joshua Matthews <jmatth93@gmail.com>
2025-03-20 16:53:35 -0500

    init commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+static void print_buf(char *buf) {
+  for (int i = 0; i<BUFSIZ; ++i) {
+    printf("%c", buf[i]);
+  }
+  printf("\n");
+}
+
+const char *request = "GET /ws HTTP/1.1\r\nHost: localhost:3000\r\nAccept: */*\r\nSec-WebSocket-Extensions: permessage-deflate\r\nSec-WebSocket-Key: 7Wrl5Wp3kkEaYOVhio4o6w==\r\nConnection: keep-alive, Upgrade\r\nSec-Fetch-Dest: empty\r\nSec-Fetch-Mode: websocket\r\nSec-Fetch-Site: same-origin\r\n\r\n";
+
+int main (int argc, char **argv) {
+  struct sockaddr_in serv_addr;
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(3000);
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  int sock = socket(serv_addr.sin_family, SOCK_STREAM, IPPROTO_TCP);
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+    fprintf(stderr, "failed to connect\n");
+    return 1;
+  }
+  char buffer[BUFSIZ] = {0};
+  size_t n = send(sock, request, strlen(request), 0);
+  if (n == 0) {
+    fprintf(stderr, "message wasn't sent\n");
+    return 1;
+  }
+  while (1) {
+    n = recv(sock, buffer, BUFSIZ, 0);
+    if (n > 0) {
+      print_buf(buffer);
+    } else {
+      fprintf(stderr, "buffer read zero\n");
+    }
+  }
+  return 0;
+}
