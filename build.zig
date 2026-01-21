926195b175f6567eab156ca3d9d820ee2a8c3e7c
Joshua Matthews <jmatth93@gmail.com>
2026-01-20 22:41:15 -0600

    get rid of wasm option for now

--- build.zig
+++ build.zig
@@ -5,8 +5,9 @@ fn createModule(
     optimize: std.builtin.OptimizeMode,
     target: std.Build.ResolvedTarget,
     use_ssl: bool,
+    disable_simd: bool,
 ) *std.Build.Module {
-    const ssl_compatible = (target.result.cpu.arch != .wasm32 and target.result.cpu.arch != .wasm64);
+    const web_target = (target.result.cpu.arch == .wasm32 or target.result.cpu.arch == .wasm64);
     const files: []const []const u8 = &.{
         "src/websocket.c",
         "src/http.c",
@@ -14,21 +15,30 @@ fn createModule(
         "src/reader.c",
         "src/protocol.c",
     };
-    const ssl_flag: []const u8 = if (use_ssl and ssl_compatible) "-DWEBC_USE_SSL=1" else "";
+    const ssl_flag: []const u8 = if (use_ssl and !web_target) "-DWEBC_USE_SSL=1" else "";
+    const simd_flag: []const u8 = if (disable_simd) "-DDISABLE_SIMD=1" else "";
+    const emscripten_flag: []const u8 = if (web_target) "-D__EMSCRIPTEN__=1" else "";
     const flags: []const []const u8 = &.{
         "-Wall",
         "-Wextra",
         "-O2",
         "-std=gnu11",
         ssl_flag,
+        simd_flag,
+        emscripten_flag,
     };
     const module = b.addModule("ws", .{
         .pic = true,
         .target = target,
         .optimize = optimize,
     });
-    if (use_ssl and ssl_compatible) {
-        module.addLibraryPath(.{.cwd_relative = "/usr/lib/x86_64-linux-gnu"});
+    if (web_target) {
+        module.addObjectFile(b.path("./deps/cstd/zig-out/lib/libwebcustom_std.a"));
+    } else {
+        module.addObjectFile(b.path("./deps/cstd/zig-out/lib/libcustom_std.a"));
+    }
+    if (use_ssl and !web_target) {
+        module.addLibraryPath(.{ .cwd_relative = "/usr/lib/x86_64-linux-gnu" });
         module.linkSystemLibrary("openssl", .{ .needed = use_ssl });
     }
     module.addCSourceFiles(.{
@@ -47,20 +57,21 @@ fn createModule(
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const use_ssl = b.option(bool, "use_ssl", "Use OpenSSL for WebSocket Secure support.") orelse false;
-    const webTarget = b.resolveTargetQuery(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
-    const webLib = b.addLibrary(.{
-        .name = "webws",
-        .linkage = .static,
-        .root_module = createModule(b, optimize, webTarget, use_ssl),
-        .use_llvm = true,
-    });
-    b.installArtifact(webLib);
+    const disable_simd = b.option(bool, "disable_simd", "Disable SIMD instructions.") orelse false;
+    //const webTarget = b.resolveTargetQuery(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
+    //const webLib = b.addLibrary(.{
+    //    .name = "webws",
+    //    .linkage = .static,
+    //    .root_module = createModule(b, optimize, webTarget, use_ssl, disable_simd),
+    //    .use_llvm = true,
+    //});
+    //b.installArtifact(webLib);
     const linkage = b.option(std.builtin.LinkMode, "linkage", "Link mode for ws library") orelse .static;
     const nativeTarget = b.standardTargetOptions(.{});
     const nativeLib = b.addLibrary(.{
         .name = "ws",
         .linkage = linkage,
-        .root_module = createModule(b, optimize, nativeTarget, use_ssl),
+        .root_module = createModule(b, optimize, nativeTarget, use_ssl, disable_simd),
     });
     b.installArtifact(nativeLib);
 }
